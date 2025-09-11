1adf47c73e14e34b293bb47438959cb9f9fbfbd1
LonDev <dev@lenkavets.ru>
2025-09-11 16:50:44 +0300

    feat(modal): add modal window to page

---
+++ src/js/modules/modals.js
@@ -0,0 +1,50 @@
+const modals = () => {
+    function bindModal(triggerSelector, modalSelector, closeSelector) {
+        const triggers = document.querySelectorAll(triggerSelector), // Переименовал для ясности
+            modal = document.querySelector(modalSelector),
+            close = document.querySelector(closeSelector);
+
+        if (!modal) {
+            console.error(`Modal with selector "${modalSelector}" not found.`);
+            return;
+        }
+
+        triggers.forEach(item => {
+            item.addEventListener('click', (e) => {
+                if (e.target) {
+                    e.preventDefault();
+                }
+
+                modal.style.display = "block";
+                document.body.style.overflow = "hidden";
+            });
+        });
+
+        if (close) {
+            close.addEventListener('click', () => {
+                modal.style.display = "none";
+                document.body.style.overflow = "";
+            });
+        }
+
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal) {
+                modal.style.display = "none";
+                document.body.style.overflow = "";
+            }
+        });
+    }
+
+    function showModalByTime(selector,time){
+        setTimeout(function(){
+            document.querySelector(selector).style.display = 'block';
+            document.body.style.overflow = "hidden";
+        },time)
+    }
+
+    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    bindModal('.phone_link', '.popup', '.popup .popup_close');
+    showModalByTime('.popup',3000)
+};
+
+export default modals;
