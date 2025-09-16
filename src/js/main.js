214bb8a252143548e2bce24ee2880f6a6dbdb4eb
Ivan Lenkavets <47rex47@mail.ru>
2025-09-16 15:41:38 +0300

    feat(timer): add timer on page

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,20 @@ import modals from './modules/modals';
 import tabs from './modules/tabs'; 
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
 
 window.addEventListener('DOMContentLoaded', ()=>{
     "use strict"
     let modalState={};
+    let deadline = '2025-12-31';
+
     changeModalState(modalState);
     modals();
     tabs('.glazing_slider','.glazing_block','.glazing_content','active');
     tabs('.decoration_slider','.decoration_item div','.decoration_content > .row > div','active');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
     forms(modalState);
+    timer('.container1', deadline);
 });
 
 console.log('test');
